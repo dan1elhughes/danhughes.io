606ff0e0c172a937f97697a6b8dd3437b4735b7e
Dan Hughes <danhughes@monzo.com>
2019-06-16 12:26:45 +0100

    Move to Now from Surge

---
+++ .build/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cd ..
+
+curl -L -O https://github.com/gohugoio/hugo/releases/download/v0.55.6/hugo_0.55.6_Linux-64bit.tar.gz
+tar -xzf hugo_0.55.6_Linux-64bit.tar.gz
+
+./hugo --minify
