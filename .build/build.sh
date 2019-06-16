af52df586e69defe8510f1079211011ce9f307a4
Dan Hughes <danhughes@monzo.com>
2019-06-16 12:40:17 +0100

    Add resource compiled output

--- .build/build.sh
+++ .build/build.sh
@@ -2,7 +2,7 @@
 
 cd ..
 
-curl -L -O https://github.com/gohugoio/hugo/releases/download/v0.55.6/hugo_extended_0.55.6_Linux-64bit.tar.gz
-tar -xzf hugo_extended_0.55.6_Linux-64bit.tar.gz
+curl -L -O https://github.com/gohugoio/hugo/releases/download/v0.55.6/hugo_0.55.6_Linux-64bit.tar.gz
+tar -xzf hugo_0.55.6_Linux-64bit.tar.gz
 
 ./hugo --minify
